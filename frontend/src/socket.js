140be1dc3f53ca047095ceadbd3f79ef2511bd9f
Anil Itagi <itagianil464@gmail.com>
2024-12-28 22:21:32 +0530

    all set

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
+console.log(BASE_URL + " it is base url");
 export const initSocket = async() => {
     const options = {
         'force new connection': true,
@@ -7,5 +8,6 @@ export const initSocket = async() => {
         'timeout': 1000, // 10 seconds
         'transports': ['websocket', 'polling'],
     };
+    console.log(options);
     return io(BASE_URL, options)
 }
